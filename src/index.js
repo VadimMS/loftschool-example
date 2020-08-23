eddc28f63c09a0fe7eba89ed13d9268fe005f175
Vadim <vadimwdr@gmail.com>
2020-08-23 17:25:41 +0300

    finished homework_3

---
+++ src/index.js
@@ -0,0 +1,216 @@
+/* ДЗ 3 - работа с исключениями и отладчиком */
+
+/*
+ Задание 1:
+
+ 1.1: Функция принимает массив и фильтрующую фукнцию и должна вернуть true или false
+ Функция должна вернуть true только если fn вернула true для всех элементов массива
+
+ 1.2: Необходимо выбрасывать исключение в случаях:
+   - array не массив или пустой массив (с текстом "empty array")
+   - fn не является функцией (с текстом "fn is not a function")
+
+ Зарпещено использовать встроенные методы для работы с массивами
+
+ Пример:
+   isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
+   isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
+ */
+function isAllTrue(array, fn) {
+    if (!Array.isArray(array) || !array.length) {
+        throw new Error('empty array');
+    }
+
+    if (typeof fn !== 'function') {
+        throw new Error('fn is not a function');
+    }
+
+    for (let item of array) {
+        if (!fn(item)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+try {
+    isAllTrue([1, 2, 3, 4, 5], n => n < 10);
+} catch (e) {
+    e.message;
+}
+
+try {
+    isAllTrue([100, 2, 3, 4, 5], n => n < 10);
+} catch (e) {
+    e.message;
+}
+
+/*
+ Задание 2:
+
+ 2.1: Функция принимает массив и фильтрующую фукнцию и должна вернуть true или false
+ Функция должна вернуть true если fn вернула true хотя бы для одного из элементов массива
+
+ 2.2: Необходимо выбрасывать исключение в случаях:
+   - array не массив или пустой массив (с текстом "empty array")
+   - fn не является функцией (с текстом "fn is not a function")
+
+ Зарпещено использовать встроенные методы для работы с массивами
+
+ Пример:
+   isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
+   isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
+ */
+function isSomeTrue(array, fn) {
+    if (!Array.isArray(array) || !array.length) {
+        throw new Error('empty array');
+    }
+
+    if (typeof fn !== 'function') {
+        throw new Error('fn is not a function');
+    }
+
+    for (let item of array) {
+        if (fn(item)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+try {
+    isSomeTrue([1, 2, 30, 4, 5], n => n > 20);
+} catch (e) {
+    e.message;
+}
+
+try {
+    isSomeTrue([1, 2, 3, 4, 5], n => n > 20);
+} catch (e) {
+    e.message;
+}
+
+/*
+ Задание 3:
+
+ 3.1: Функция принимает заранее неизветсное количество аргументов, первым из которых является функция fn
+ Функция должна поочередно запустить fn для каждого переданного аргумента (кроме самой fn)
+
+ 3.2: Функция должна вернуть массив аргументов, для которых fn выбросила исключение
+
+ 3.3: Необходимо выбрасывать исключение в случаях:
+   - fn не является функцией (с текстом "fn is not a function")
+ */
+function returnBadArguments(fn, ...rest) {
+    if (typeof fn !== 'function') {
+        throw new Error('fn is not a function');
+    }
+
+    let newArr = [];
+
+    for (let item of rest) {
+        try {
+            fn(item);
+        } catch (e) {
+            e.message
+            newArr.push(item);
+        }
+    }
+
+    return newArr;
+}
+
+function foo(arg) {
+    if (typeof arg !== 'string') {
+        throw new Error('not string');
+    }
+}
+
+try {
+    returnBadArguments(foo, [1, 2, 3], 'string', 5, null);
+} catch (e) {
+    e.message;
+}
+
+/*
+ Задание 4:
+
+ 4.1: Функция имеет параметр number (по умолчанию - 0)
+
+ 4.2: Функция должна вернуть объект, у которого должно быть несколько методов:
+   - sum - складывает number с переданными аргументами
+   - dif - вычитает из number переданные аргументы
+   - div - делит number на первый аргумент. Результат делится на следующий аргумент (если передан) и так далее
+   - mul - умножает number на первый аргумент. Результат умножается на следующий аргумент (если передан) и так далее
+
+ Количество передаваемых в методы аргументов заранее неизвестно
+
+ 4.3: Необходимо выбрасывать исключение в случаях:
+   - number не является числом (с текстом "number is not a number")
+   - какой-либо из аргументов div является нулем (с текстом "division by 0")
+ */
+function calculator(number = 0) {
+    if (!Number.isFinite(number)) {
+        throw new Error('number is not a number');
+    }
+
+    return {
+        sum(...val) {
+            let result = number;
+
+            for (let el of val) {
+                result += el;
+            }
+
+            return result;
+        },
+        dif(...val) {
+            let result = number;
+
+            for (let el of val) {
+                result -= el;
+            }
+
+            return result;
+        },
+        div(...val) {
+            let result = number;
+
+            for (let el of val) {
+                if (el === 0) {
+                    throw new Error('division by 0');
+                }
+
+                result /= el;
+            }
+
+            return result;
+        },
+        mul(...val) {
+            let result = number;
+
+            for (let el of val) {
+                result *= el;
+            }
+
+            return result;
+        },
+    };
+}
+
+try {
+    let calc = calculator(10);
+    
+    calc.sum(10, 5, 10);
+    calc.dif(15, 5);
+    calc.div(2, 2, 3);
+    calc.mul(2);
+} catch (e) {
+    e.message;
+}
+
+/* При решении задач, пострайтесь использовать отладчик */
+
+export { isAllTrue, isSomeTrue, returnBadArguments, calculator };
